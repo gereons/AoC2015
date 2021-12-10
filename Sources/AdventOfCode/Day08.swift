300231338cecfc3c170d58526a281cf3b4498780
Gereon Steffens <gereon@steffens.org>
2021-12-11 00:59:50 +0100

    day 8 + 9

--- Sources/AdventOfCode/Day08.swift
+++ Sources/AdventOfCode/Day08.swift
@@ -3,27 +3,71 @@
 
 struct Day08 {
     let day = "08"
-    let testData = [ "1","2","3" ]
+    let testData = [
+        #""""#,
+        #""abc""#,
+        #""aaa\"aaa""#,
+        #""\x27""#
+    ]
 
     func run() {
         // let data = testData
         let data = readFile(named: "Day\(day)_input.txt")
 
-        let positions = Timer.time(day) {
-            data.compactMap { Int($0) }
+        print("Solution for part 1: \(part1(data))")
+        print("Solution for part 2: \(part2(data))")
+    }
+
+    private func part1(_ data: [String]) -> Int {
+        let timer = Timer(day); defer { timer.show() }
+
+        var chars = 0
+        var inMemory = 0
+        for line in data {
+            chars += line.count
+
+            var memChars = 0
+            var index = line.index(line.startIndex, offsetBy: 1)
+            var nextIndex = line.index(after: index)
+            while nextIndex != line.endIndex {
+                memChars += 1
+                let nextCh = line[nextIndex]
+                if line[index] == "\\" {
+                    var skip = 0
+                    if nextCh == "\\" || nextCh == "\"" { skip = 1 }
+                    if nextCh == "x" { skip = 3 }
+                    nextIndex = line.index(nextIndex, offsetBy: skip)
+                }
+
+                index = nextIndex
+                nextIndex = line.index(after: nextIndex)
+            }
+
+            inMemory += memChars
+
+            // print(line, line.count, memChars)
+        }
+        return chars - inMemory
+    }
+
+    private func part2(_ data: [String]) -> Int {
+        let timer = Timer(day); defer { timer.show() }
+
+        var chars = 0
+        var encoded = 0
+        for line in data {
+            chars += line.count
+            var enc = line.count + 2
+
+            for ch in line {
+                if ch == "\"" || ch == "\\" {
+                    enc += 1
+                }
+            }
+            encoded += enc
+            // print(line, line.count, enc)
         }
 
-        print("Solution for part 1: \(part1(positions))")
-        print("Solution for part 2: \(part2(positions))")
-    }
-
-    private func part1(_ positions: [Int]) -> Int {
-        let timer = Timer(day); defer { timer.show() }
-        return 42
-    }
-
-    private func part2(_ positions: [Int]) -> Int {
-        let timer = Timer(day); defer { timer.show() }
-        return 42
+        return encoded - chars
     }
 }
--- Sources/AdventOfCode/Day09.swift
+++ Sources/AdventOfCode/Day09.swift
@@ -3,27 +3,114 @@
 
 struct Day09 {
     let day = "09"
-    let testData = [ "1","2","3" ]
+    let testData = [
+        "London to Dublin = 464",
+        "London to Belfast = 518",
+        "Dublin to Belfast = 141"
+    ]
+
+    struct City: Hashable {
+        static var distances = [String: Int]()
+
+        let name: String
+
+        func distance(to city: City) -> Int {
+            return Self.distances["\(self.name):\(city.name)"]!
+        }
+    }
+
+    class Route {
+        let cities: [City]
+
+        var _distance: Int?
+        var distance: Int {
+            if _distance == nil {
+                _distance = calculateDistance()
+            }
+            return _distance ?? 0
+        }
+
+        init(cities: [City]) {
+            self.cities = cities
+        }
+
+        private func calculateDistance() -> Int {
+            var result = 0
+            var previousCity: City?
+
+            cities.forEach { city in
+                if let previous = previousCity {
+                    result += previous.distance(to: city)
+                }
+                previousCity = city
+            }
+
+            return result
+        }
+    }
 
     func run() {
         // let data = testData
         let data = readFile(named: "Day\(day)_input.txt")
 
-        let positions = Timer.time(day) {
-            data.compactMap { Int($0) }
+        let (distances, cities) = Timer.time(day) { () -> ([String: Int], [City]) in
+            var dist = [String: Int]()
+            var cities = Set<City>()
+            for line in data {
+                let c = line.split(separator: " ")
+                let city1 = String(c[0])
+                let city2 = String(c[2])
+                let distance = Int(c[4])
+                dist["\(city1):\(city2)"] = distance
+                dist["\(city2):\(city1)"] = distance
+                cities.insert(City(name: city1))
+                cities.insert(City(name: city2))
+            }
+            return (dist, Array(cities))
+        }
+        City.distances = distances
+
+        let (min, max) = part1And2(cities)
+        print("Solution for part 1: \(min)")
+        print("Solution for part 2: \(max)")
+    }
+
+    private func part1And2(_ cities: [City]) -> (Int, Int) {
+        let timer = Timer(day); defer { timer.show() }
+
+        var minDistance = Int.max
+        var maxDistance = 0
+        cities.permutations { cities in
+            let route = Route(cities: cities)
+            minDistance = min(minDistance, route.distance)
+            maxDistance = max(maxDistance, route.distance)
         }
 
-        print("Solution for part 1: \(part1(positions))")
-        print("Solution for part 2: \(part2(positions))")
+        return (minDistance, maxDistance)
+    }
+}
+
+extension Array {
+    // https://en.wikipedia.org/wiki/Heap%27s_algorithm
+
+    func permutations(closure: ([Element]) -> Void) {
+        var data = self
+        generate(n: data.count, data: &data, closure: closure)
     }
 
-    private func part1(_ positions: [Int]) -> Int {
-        let timer = Timer(day); defer { timer.show() }
-        return 42
+    private func generate(n: Int, data: inout [Element], closure: ([Element]) -> Void) {
+        if n == 1 {
+            closure(data)
+        } else {
+            for i in 0 ..< n {
+                generate(n: n - 1, data: &data, closure: closure)
+                if n % 2 == 0 {
+                    data.swapAt(i, n - 1)
+                } else {
+                    data.swapAt(0, n - 1)
+                }
+            }
+        }
     }
 
-    private func part2(_ positions: [Int]) -> Int {
-        let timer = Timer(day); defer { timer.show() }
-        return 42
-    }
 }
